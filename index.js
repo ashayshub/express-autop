314e211e51a0607956786424939d0ee3ca4cdb82
Ashay <ashay@flux7.com>
2017-12-03 10:06:23 +0530

    Added initial bootstrap files

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 'use strict';
-const express = require('express' 4.16.2)
-const app = 	express()
+const express = require('express')
+const app 	  = express()
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.use('/static', express.static('static'))
 
-app.listen(5000, ()=> console.log('Server started, listening on port 50000'))
+app.set('view engine', 'pug')
+app.set('views', './templates')
+
+// All GET Routes
+app.get('/', (req, res) => {
+	var car_type = req.query.car_type || 'Truck'	
+	var allowed_cars = ['Truck', 'Sport']
+
+	if(allowed_cars.indexOf(car_type) == -1){
+		res.status(400).send('Bad Request')
+		return
+	}
+	var place_holder = {
+		'car_type': car_type,
+		'page': 1,
+		'per_page': 10,
+		'cars': [{
+			'car_type': 'Truck',
+			'title': 'Some Car',
+			'summary': 'A good car',
+			'price_query': 'test'
+		}
+		]
+	}
+	res.render('main', place_holder)
+})
+
+app.get('/price', (req, res) => {})
+
+
+// All POST Routes
+app.post('/populate', (req, res) => res.render('main'))
+
+
+// All delete Routes
+app.get('/teardown', (req, res) => res.render('main'))
+
+app.listen(5000, ()=> console.log('Server started, listening on port 5000'))
---
+++ static/js/autop.js
@@ -0,0 +1,75 @@
+function printToModal(title, data) {
+   $('.modal-title').text(title);
+   $('.modal-body p').text(data);
+   $('#myModal').modal('show');
+   return true;
+}
+
+
+$(document).ready(function() {
+    
+    $("#populate").click(function(event){
+        
+        event.preventDefault();
+        var modal_title = "Populate Database";
+        $('.modal-title').text(modal_title);
+        $('.modal-body p').text('Populating... it may take 10-15 seconds');
+        var saveData = $.ajax({
+              type: 'POST',
+              url: "/populate/",
+              success: function(resultData){
+                   printToModal(modal_title, resultData);
+                   location.href = "/";
+              }
+        });
+        saveData.error(function() {
+              printToModal(modal_title, 'Error: Something went wrong while populating data');
+        });
+    });
+
+    $("#teardown").click(function(){
+        var modal_title = "Drop table";
+        $('.modal-title').text(modal_title);
+        $('.modal-body p').text('Tearing down...');
+        $('#modal-close').attr('data-teardown', '1');
+        var saveData = $.ajax({
+              type: 'DELETE',
+              url: "/teardown/",
+              success: function(resultData){
+                   printToModal(modal_title, resultData);
+              }
+        });
+        saveData.error(function() {
+              printToModal(modal_title, "Error: Something went wrong while dropping table");
+        });
+     });
+
+    $("[id^=price-info-]").click(function(){
+        var modal_title = "Vehicle Price";
+        var price_query = $(this).data("price-query");
+        $('.modal-title').text(modal_title);
+        $('.modal-body p').text('Fetching price for vehicle...');
+
+        var saveData = $.ajax({
+              type: 'GET',
+              url: "/price?price_query=" + encodeURIComponent(price_query),
+              success: function(resultData){
+                   body_text = "Base price of the vehicle is $" + resultData.baseMSRP;
+                   printToModal(modal_title, body_text);
+              }
+        });
+        saveData.error(function() {
+              printToModal(modal_title, 'Error: Could not fetch the appropriate price');
+        });
+     });
+
+     $('#myModal').on('hide.bs.modal', function(){
+        var refresh = $("#modal-close").data("teardown");
+        if (refresh == "1") {
+           $("#modal-close").removeAttr('data-teardown');
+           location.href = "/";
+        }
+     });
+
+});
+
